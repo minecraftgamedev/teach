52018bd6374a944bea7031d0a54d000c1c7aa8d7
tazadejava <tonitruirrein@gmail.com>
2020-08-15 21:58:49 -0700

    Updates

---
+++ precache-manifest.eb5dbc821d24b459497899522a3fb4d5.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d8d9173d36794452150aa8d6f661663",
+    "url": "/teaching-resources/index.html"
+  },
+  {
+    "revision": "3c102509a81c2d3cedbc",
+    "url": "/teaching-resources/static/css/main.95c664fc.chunk.css"
+  },
+  {
+    "revision": "17e9baaa2514d3c4f2ef",
+    "url": "/teaching-resources/static/js/2.284cd7bf.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/teaching-resources/static/js/2.284cd7bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c102509a81c2d3cedbc",
+    "url": "/teaching-resources/static/js/main.1ed84c26.chunk.js"
+  },
+  {
+    "revision": "f20ed899a0d597ba2c43",
+    "url": "/teaching-resources/static/js/runtime-main.bc784503.js"
+  },
+  {
+    "revision": "8a21cd6fedf289a3c060067a5c4a4817",
+    "url": "/teaching-resources/static/media/challenge1.8a21cd6f.md"
+  },
+  {
+    "revision": "0d869a89dddf8f4aa8d33293eb3af4e4",
+    "url": "/teaching-resources/static/media/challenge2.0d869a89.md"
+  },
+  {
+    "revision": "475e0dc5739724f6a29fc92387b70e06",
+    "url": "/teaching-resources/static/media/challenge3.475e0dc5.md"
+  },
+  {
+    "revision": "d66eebd48afbfeb6ce92d0247f15a178",
+    "url": "/teaching-resources/static/media/challenge4.d66eebd4.md"
+  },
+  {
+    "revision": "c2b572faf63e37a31faae7bd3cb72088",
+    "url": "/teaching-resources/static/media/challenge5.c2b572fa.md"
+  },
+  {
+    "revision": "48e4d1d9676aabc6b2b7af907460cfd9",
+    "url": "/teaching-resources/static/media/challenge6.48e4d1d9.md"
+  },
+  {
+    "revision": "dd4f1638178c241acba465e7e2f79c34",
+    "url": "/teaching-resources/static/media/development1.dd4f1638.md"
+  },
+  {
+    "revision": "cb97c0923bffad7869bdf555c043b16f",
+    "url": "/teaching-resources/static/media/development2.cb97c092.md"
+  },
+  {
+    "revision": "2a404623d8ba596ba5658a5c1dfbb23a",
+    "url": "/teaching-resources/static/media/development3.2a404623.md"
+  },
+  {
+    "revision": "b0522406c2259a236ecd0654b406e316",
+    "url": "/teaching-resources/static/media/development4.b0522406.md"
+  },
+  {
+    "revision": "1a0f549d54ef9786c49bf9ab8b931b6e",
+    "url": "/teaching-resources/static/media/development5.1a0f549d.md"
+  },
+  {
+    "revision": "73211ebe6dfd50fd11bb4416ff3748e8",
+    "url": "/teaching-resources/static/media/development6.73211ebe.md"
+  },
+  {
+    "revision": "5ddd8ed35d02eca27896e3463e135397",
+    "url": "/teaching-resources/static/media/important information.5ddd8ed3.md"
+  },
+  {
+    "revision": "f118211d20b99c4b737dae391fd619c2",
+    "url": "/teaching-resources/static/media/past presentations.f118211d.md"
+  },
+  {
+    "revision": "732fa84d3e3eda73b3fcb6aee4aa5382",
+    "url": "/teaching-resources/static/media/server environment.732fa84d.md"
+  }
+]);
